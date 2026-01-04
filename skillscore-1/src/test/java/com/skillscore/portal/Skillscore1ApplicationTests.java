f78ac9bc98084d7e1c1c660c52b146073636d09c
asish123-tech <sahuasish801@gmail.com>
2026-01-04 12:23:34 +0530

    run commit

---
+++ skillscore-1/src/test/java/com/skillscore/portal/Skillscore1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillscore.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skillscore1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
